d86f9507ea5d08fef92d205d8bd2348230bd8cab
Emily Coxon <emilyecoxon@gmail.com>
2023-01-26 12:11:28 +0800

    Completed jora notebook, turned scrape and word count dfs into csv files, created tables in pgadmin, loaded dfs into pgadmin tables

---
+++ w3/Jora_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE "Jora_Scrape" (
+	"job_title" VARCHAR NOT NULL,
+	"company_name" VARCHAR NOT NULL,
+	"short_description" VARCHAR NOT NULL
+)
+
+CREATE TABLE "Jora_Word_Count" (
+	"words" VARCHAR NOTNULL,
+	"count" INT NOTNULL
+)
