7f727eecb77fa7ef1449c82e56354a7272f20a64
Emily Coxon <emilyecoxon@gmail.com>
2023-01-27 17:11:36 +0800

    added some more relavent text to the flask app but could be off track..

--- app.py
+++ app.py
@@ -1,23 +1,87 @@
-# 1. import Flask
-from flask import Flask
+import numpy as np
 
-# 2. Create an app, being sure to pass __name__
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///<OUR_DATABASE_HERE>.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(autoload_with=engine)
+
+# Save reference to the table
+Passenger = Base.classes.passenger ## THIS IS FROM WK 10.10.3 class solutions, would passenger need to be an individual ID for each of the job postings?
+
+#################################################
+# Flask Setup
+#################################################
 app = Flask(__name__)
 
-# 3. Define what to do when a user hits the index route
+
+#################################################
+# Flask Routes
+#################################################
+
 @app.route("/")
-def home():
-    print("Server recieved request for 'Home' page...") #prints to the console
-    return "Welcome to my home page!" #returned to the client
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/industry<br/>"
+        f"/api/v1.0/company_name"
+        f"/api/v1.0/job_title"
+        f"/api/v1.0/short_description"          ##### edited this, not sure it's correct
+    )
 
 
-# 4. Define what to do when a user hits the /about route
-@app.route("/about")
-def about():
-    print("Server recieved request for 'About' page...")
-    return "Welcome to about page!"
+@app.route("/api/v1.0/company_name")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all company names"""          ## Not sure what to do here, maybe company_name?
+    # Query all companies
+    results = session.query(Jora_schema.company_name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
 
 
-if __name__ == "__main__": 
-    app.run(debug=True) #runs the app on a local development server
-    #makes dev. much easier, in production though best practices means debug must =False
+@app.route("/api/v1.0/passengers")                      ## got to here
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
