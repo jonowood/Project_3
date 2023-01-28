88c619ee196dc4096974ac98be9fac8c168b089b
jonowood <113280744+jonowood@users.noreply.github.com>
2023-01-28 22:18:24 +0800

    Create Project 3 Schema.sql

---
+++ Database Schema/Project 3 Schema.sql
@@ -0,0 +1,154 @@
+DROP TABLE IF EXISTS Sales_Word_Count, Community_Services_Word_Count, Healthcare_Word_Count, Construction_Word_Count, Engineering_Word_Count;
+DROP TABLE IF EXISTS Construction_Location_Count, Engineering_Location_Count, Healthcare_Location_Count, Sales_Location_Count, Community_Services_Location_Count;
+DROP TABLE IF EXISTS Construction_Scrapes, Engineering_Scrapes, Healthcare_Scrapes, Sales_Scrapes, Community_Service_Scrapes;
+DROP TABLE IF EXISTS Job_ID CASCADE;
+
+
+-- Create tables for SCRAPED DATA
+
+-- JOB_ID
+
+CREATE TABLE Job_ID (
+	job_id INT PRIMARY KEY NOT NULL,
+	job_field VARCHAR(30),
+	job_location VARCHAR(30)
+);
+
+-- CONSTRUCTION_SCRAPES
+
+CREATE TABLE Construction_Scrapes (
+	job_id INT,
+	job_title VARCHAR(30),
+	company_name VARCHAR(30),
+	short_description TEXT,
+	job_location VARCHAR(30),
+	job_classification VARCHAR(30),
+	job_subclassification VARCHAR(30),
+	job_site VARCHAR(20),
+	FOREIGN KEY (job_id) REFERENCES Job_ID(job_id)
+);
+
+-- ENGINEERING_SCRAPES
+
+CREATE TABLE Engineering_Scrapes (
+	job_id INT,
+	job_title VARCHAR(30),
+	company_name VARCHAR(30),
+	short_description TEXT,
+	job_location VARCHAR(30),
+	job_classification VARCHAR(30),
+	job_subclassification VARCHAR(30),
+	job_site VARCHAR(20),
+	FOREIGN KEY (job_id) REFERENCES Job_ID(job_id)
+);
+
+-- HEALTHCARE_SCRAPES
+
+CREATE TABLE Healthcare_Scrapes (
+	job_id INT,
+	job_title VARCHAR(30),
+	company_name VARCHAR(30),
+	short_description TEXT,
+	job_location VARCHAR(30),
+	job_classification VARCHAR(30),
+	job_subclassification VARCHAR(30),
+	job_site VARCHAR(20),
+	FOREIGN KEY (job_id) REFERENCES Job_ID(job_id)
+);
+
+-- SALES_SCRAPES
+
+CREATE TABLE Sales_Scrapes (
+	job_id INT,
+	job_title VARCHAR(30),
+	company_name VARCHAR(30),
+	short_description TEXT,
+	job_location VARCHAR(30),
+	job_classification VARCHAR(30),
+	job_subclassification VARCHAR(30),
+	job_site VARCHAR(20),
+	FOREIGN KEY (job_id) REFERENCES Job_ID(job_id)
+);
+
+-- COMMUNITY_SERVICES_SCRAPES
+
+CREATE TABLE Community_Service_Scrapes (
+	job_id INT,
+	job_title VARCHAR(30),
+	company_name VARCHAR(30),
+	short_description TEXT,
+	job_location VARCHAR(30),
+	job_classification VARCHAR(30),
+	job_subclassification VARCHAR(30),
+	job_site VARCHAR(20),
+	FOREIGN KEY (job_id) REFERENCES Job_ID(job_id)
+);
+
+-- Create tables for LOCATION COUNT
+
+CREATE TABLE Construction_Location_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	count INT
+);
+
+CREATE TABLE Engineering_Location_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	count INT
+);
+
+CREATE TABLE Healthcare_Location_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	count INT
+);
+
+CREATE TABLE Sales_Location_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	count INT
+);
+
+CREATE TABLE Community_Services_Location_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	count INT
+);
+
+-- Create tables for WORD COUNT
+
+CREATE TABLE Sales_Word_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	word VARCHAR(15),
+	count INT
+);
+
+CREATE TABLE Community_Services_Word_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	word VARCHAR(15),
+	count INT
+);
+
+CREATE TABLE Healthcare_Word_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	word VARCHAR(15),
+	count INT
+);
+
+CREATE TABLE Construction_Word_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	word VARCHAR(15),
+	count INT
+);
+
+CREATE TABLE Engineering_Word_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	word VARCHAR(15),
+	count INT
+);
