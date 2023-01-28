e83c750c6982ff4bbe019e38219df737bc40d759
jonowood <113280744+jonowood@users.noreply.github.com>
2023-01-28 23:09:27 +0800

    schema and erd updated, completed tables in nb

--- Database Schema/Project 3 Schema.sql
+++ Database Schema/Project 3 Schema.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS Sales_Word_Count, Community_Services_Word_Count, Healthcare_Word_Count, Construction_Word_Count, Engineering_Word_Count;
+DROP TABLE IF EXISTS Sales_Word_Count, Community_Services_Word_Count, Healthcare_Word_Count, Construction_Word_Count, Engineering_Word_Count, All_Fields_Word_Count;
 DROP TABLE IF EXISTS Construction_Location_Count, Engineering_Location_Count, Healthcare_Location_Count, Sales_Location_Count, Community_Services_Location_Count;
 DROP TABLE IF EXISTS Construction_Scrapes, Engineering_Scrapes, Healthcare_Scrapes, Sales_Scrapes, Community_Service_Scrapes;
 DROP TABLE IF EXISTS Job_ID CASCADE;
@@ -18,12 +18,12 @@ CREATE TABLE Job_ID (
 
 CREATE TABLE Construction_Scrapes (
 	job_id INT,
-	job_title VARCHAR(30),
-	company_name VARCHAR(30),
+	job_title TEXT,
+	company_name VARCHAR,
 	short_description TEXT,
-	job_location VARCHAR(30),
-	job_classification VARCHAR(30),
-	job_subclassification VARCHAR(30),
+	job_location VARCHAR,
+	job_classification VARCHAR,
+	job_subclassification VARCHAR,
 	job_site VARCHAR(20),
 	FOREIGN KEY (job_id) REFERENCES Job_ID(job_id)
 );
@@ -147,6 +147,13 @@ CREATE TABLE Construction_Word_Count (
 );
 
 CREATE TABLE Engineering_Word_Count (
+	job_field VARCHAR(30),
+	job_location VARCHAR(30),
+	word VARCHAR(15),
+	count INT
+);
+
+CREATE TABLE All_Fields_Word_Count (
 	job_field VARCHAR(30),
 	job_location VARCHAR(30),
 	word VARCHAR(15),
